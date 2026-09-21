2da00be2bb0ff88d6322346837013500634261ca
wsenodeb2026 <dmswmtnl25@gmail.com>
2026-09-21 12:05:07 +0530

    Update server.js

--- newback/server.js
+++ newback/server.js
@@ -1,1 +1,18 @@
 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res) => {
+    res.send("Dynamic Excel Backend is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
